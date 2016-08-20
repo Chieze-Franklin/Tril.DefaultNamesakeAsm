4245c90b545df04ef42f3752d65279fe3c41264f
Franklin Chieze <chieze.franklin@gmail.com>
2016-08-20 16:58:57 +0100

    First Commit

---
+++ DefaultNamesakeAsm/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tril.Attributes;
+
+namespace System
+{
+    [Annotation("java.lang", "android", "java")]
+    [TypeName("string", "java")]
+    public class String
+    {
+        [Annotation("hahaha", "android", "java")]
+        [Comment("native", "android", "java")]
+        [AccessModifier( AccessModifiers.Internal)]
+        public string Normalize() { return null; }
+        public System.Boolean IsNormalized([ParamType("sys.txt::NormalizationForm")]System.Text.NormalizationForm normalizationForm) 
+        {
+            return false; 
+        }
+    }
+}
